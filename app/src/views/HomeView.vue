9cacb42d4baf4c1df50f75d128ead62f7648cfb3
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2025-03-27 12:30:36 -0400

    sfa

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+
+<!--
+typescript
+- defines interfaces
+- clear defs of interfaces
+
+-->
