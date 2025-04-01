e2dba0608f6348a5b3ce82b999e648a6d556c314
Judy <158211720+judyyan27@users.noreply.github.com>
2025-04-01 11:52:12 -0400

    edit file

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
     <TheWelcome />
   </main>
 </template>
-
 
 <!--
 typescript
