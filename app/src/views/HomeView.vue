bd033ed4d7974b8f098befd6230d4d8e7edf2519
Judy <158211720+judyyan27@users.noreply.github.com>
2025-04-11 12:24:39 -0400

    Revert "Update HomeView.vue"
    This reverts commit fe80caa81291bfa2a2fee54e2bada9791fff9928.

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,25 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <div>
-      <form action="">
-        <label for="name">Username:</label>
-        <input type="text" name="username" id="name" />
-        <label for="pass">Password:</label>
-        <input type="text" name="password" id="pass" />
-        <div>
-          <label for="name">Username: </label>
-          <input type="text" name="username" id="name" />
-        </div>
-        <div>
-          <label for="pass">Password: </label>
-          <input type="password" name="password" id="pass" />
-        </div>
-        <button id="LogIn" type="submit">Submit</button>
-      </form>
-    </div>
-  </main>
+  <main></main>
 </template>
 
 <!--
