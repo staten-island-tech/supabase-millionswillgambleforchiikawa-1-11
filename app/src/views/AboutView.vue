bc57aa9c38837b7986d923ac7e5d7a9226b0a139
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2025-05-19 12:30:44 -0400

    dfsg

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">text</div>
+  <div></div>
 </template>
 
-<style></style>
+<script setup lang="ts">
+import ChiikawaIdle from '@pub'
+</script>
+<style>
+body {
+  min-height: 100dvh;
+  background: url(https://i.pinimg.com/236x/dc/ff/0c/dcff0c4e41a322e809e148396886118e.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
