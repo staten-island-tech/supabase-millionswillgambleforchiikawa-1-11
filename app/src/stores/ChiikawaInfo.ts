1a91d6e972dd499dd77ab95a4a5bcefeab0af676
Judy <158211720+judyyan27@users.noreply.github.com>
2025-05-29 12:32:12 -0400

    data

---
+++ app/src/stores/ChiikawaInfo.ts
@@ -0,0 +1,15 @@
+import type { GachaItem } from '../stores/gachaStores'
+
+export const gachaPool: GachaItem[] = [
+  { id: 1, name: 'Chiikawa', rarity: 5, image: '', description:"Chiikawa is gentle, sweet, and kind. Cautious to step out of their comfort zone, and somewhat shy, Chiikawa often gets nervous which usually results in anxious tears. Thankfully, best friends Hachiware and Usagi always has this cute and small creature’s back."},
+  { id: 2, name: 'Hachiware', rarity: 5, image: '', description: "Hachiware has a big heart, possesses a can-do attitude, and always finds the bright side. A true best friend, Hachiware helps Chiikawa overcome fears and makes them feel safe. Despite the cat-like ears and the occasional blep, for the record, Hachiware is not necessarily a cat."},
+  { id: 3, name: 'Usagi', rarity: 5, image: '', description: "The most energetic of the crew, Usagi seems to find the fun. Though the word usagi literally means rabbit in Japanese, and Usagi has bunny-looking ears, it’s unclear Usagi’s identity. Maybe a rabbit, maybe not. Lively and fearless, Usagi is known for signature yells “Yaha!” and “Ura!”. Interestingly, no one knows where Usagi lives."},
+  { id: 4, name: '', rarity: 0, image: '', description: ''},
+  { id: 5, name: '', rarity: 0, image: '', description: ''},
+  { id: 6, name: '', rarity: 0, image: '', description: ''},
+  { id: 7, name: '', rarity: 0, image: '', description: ''},
+  { id: 8, name: '', rarity: 0, image: '', description: ''},
+  { id: 9, name: '', rarity: 0, image: '', description: ''},
+  { id: 10, name: '', rarity: 0, image: '', description: ''},
+  { id: 11, name: '', rarity: 0, image: '', description: ''},
+]
